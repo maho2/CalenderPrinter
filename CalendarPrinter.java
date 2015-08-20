d958d77cc87e26b18aca93ac803be1a9d4be091e
maho2 <maho1442@yahoo.com>
2015-08-20 18:59:37 +0000

    This is your commit message

---
+++ CalendarPrinter.java
@@ -0,0 +1,11 @@
+public class CalendarPrinter
+{
+        public static void main (String[] args) {
+            int x = 1;
+            while (x < 31 )
+            {
+                System.out.println ( "September" + x);
+                x++;
+            }
+    }
+}
---
+++ CalenderPrinter.java
@@ -0,0 +1,10 @@
+public class CalendarPrinter
+{
+        public static void main (String[] args) {
+            int x = 4;
+            if (x > 2)
+            {
+                system.out.println ("yes");
+            }
+    }
+}
