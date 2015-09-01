346f089ec5077240b01b9fe6265859dbc1a6184b
maho2 <maho1442@yahoo.com>
2015-09-01 18:52:45 +0000

    CalendarPrinter

--- CalendarPrinter.java
+++ CalendarPrinter.java
@@ -1,11 +1,26 @@
 public class CalendarPrinter
 {
-        public static void main (String[] args) {
-            int x = 1;
-            while (x < 31 )
+    public static void main (String[] args) {
+        int x = 1;
+        while (x < 31)
+        {
+            if (x==5||x==6||x==12||x==13||x==19||x==20||x==26||x==27)
             {
-                System.out.println ( "September" + x);
-                x++;
+                System.out.println("September " + x + " - Weekend");
             }
+            else if (x==7)
+            {
+                System.out.println("September " + x + " - No School (Labor Day)");
+            }
+            else if (x==25)
+            {
+                System.out.println("September " + x + " -End of the marking period");
+            }
+            else
+            {
+                System.out.println("September " + x );
+            }
+            x++;
+        }
     }
 }
